2d8edf77fd187b56be8373d881c8cfe3796a82a4
tcorti <tcorti17@gmail.com>
2017-12-04 00:50:21 -0300

    cambios menores en frontend

--- TP2-Frontend/Frontend/src/app/app.module.ts
+++ TP2-Frontend/Frontend/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from 'app/Header/header.component';
 import { PartidosComponent } from 'app/Partidos/partidos.component';
 import { ListaPartidoComponent } from 'app/Partidos/ListadoPartidos/lista-partido.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     PartidosComponent,
     ListaPartidoComponent
   ],
